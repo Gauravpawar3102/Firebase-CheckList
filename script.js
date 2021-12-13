6bcaea2997c20166bcf4a0272b74d18e55169306
Gaurav <gauravpawar3102@gmail.com>
2021-12-13 20:16:22 +0530

    df

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
+// import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
 
-import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
-const firebaseConfig = {
-  apiKey: 'AIzaSyCRlQv7O7mZuQ5Od9WTN2Bw_VHLKw1NVmc',
-  authDomain: 'check-4295c.firebaseapp.com',
-  projectId: 'check-4295c',
-  storageBucket: 'check-4295c.appspot.com',
-  messagingSenderId: '383887812191',
-  appId: '1:383887812191:web:5e2b7fc1363299df385dea',
-  measurementId: '${config.measurementId}',
-};
+// import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics.js';
+// import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyCRlQv7O7mZuQ5Od9WTN2Bw_VHLKw1NVmc',
+//   authDomain: 'check-4295c.firebaseapp.com',
+//   projectId: 'check-4295c',
+//   storageBucket: 'check-4295c.appspot.com',
+//   messagingSenderId: '383887812191',
+//   appId: '1:383887812191:web:5e2b7fc1363299df385dea',
+//   measurementId: '${config.measurementId}',
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore();
-// References
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const db = getFirestore();
+// // References
